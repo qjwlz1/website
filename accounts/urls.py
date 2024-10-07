92204d5f1efdbe26b5554df924c255e9d47bcde0
qjwlz <qjwlz@ya.ru>
2024-10-07 23:12:48 +0300

    Fix URL patterns in accounts/urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,12 @@
+from django.contrib import admin
 from django.urls import path, include
 from . import views  # Импортируем views из текущего приложения
-from django.contrib.auth import views as auth_views
-
 
 urlpatterns = [
+    path('admin/', admin.site.urls),  # Подключаем маршруты для админки
     path('login/', views.login_page, name='login'),  # Маршрут для страницы входа
     path('register/', views.register_page, name='register'),  # Маршрут для страницы регистрации
     path('logout/', views.logout_user, name='logout'),  # Добавляем маршрут для выхода
     path('', views.index, name='index'),  # Маршрут для главной страницы
-    #path('accounts/', include('accounts.urls')),  # Подключаем маршруты приложения accounts
-    
+    path('accounts/', include('accounts.urls')),  # Подключаем маршруты приложения accounts
 ]
